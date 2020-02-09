ecdae72aaa4df3fc9579a5ff4b78a73c423987e4
DanielAcero <daniacerocotacio@gmail.com>
2020-02-09 00:34:17 -0500

    iniciando proyecto

---
+++ dist/index.js
@@ -0,0 +1,29 @@
+"use strict";
+//declarar elementos
+const slider = document.querySelector('#slider');
+const canvas = document.querySelector('#canvas');
+//agg eventos al slider
+slider.addEventListener('input', handleInputSlider);
+const ctx = canvas.getContext('2d');
+const images = [];
+window.addEventListener('load', pageLoaded);
+function pageLoaded() {
+    for (let index = 1; index <= 30; index++) {
+        const number = index.toString().padStart(2, '00');
+        const url = `./img/IMG_${number}.jpg`;
+        const image = new Image();
+        image.src = url;
+        image.addEventListener('load', () => {
+            images[index] = image;
+            if (index === 1) {
+                loadImage(index);
+            }
+        });
+    }
+}
+function loadImage(index) {
+    ctx.drawImage(images[index], 0, 0, canvas.width, canvas.height);
+}
+function handleInputSlider() {
+    loadImage(this.value);
+}
